ed8084b0703b6d88038bd1f3f552eaee6ea1cac4
Arvin San Andres <arvinsanandres@aveco.com.ph>
2017-09-06 17:25:06 +0800

    cleaned netmon.go

--- netmon.go
+++ netmon.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"time"
 )
 
 const (
@@ -21,7 +20,7 @@ const (
 	TIME_FORMAT 		= "2006-01-02 15:04:05"	// how datetime is formatted
 
 	STATIC_PATH 		= "/public/"	// URL css/js folder
-	STATIC_DIR			= "public"		// folder name
+	STATIC_DIR			= "./public"	// folder name
 )
 
 var addr = flag.String("addr", ":9000", "http service address")
@@ -111,7 +110,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-<<<<<<< HEAD
 	sess, err := netmonSess.Get(r, "netmon")
 	if err != nil {
 		http.Error(w, err.Error(), 400)
@@ -127,15 +125,6 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, "/", 301)
-=======
-	sessionOld, err := netmonSessions.Get(r, "netmon")
-	fmt.Println("Session in logout")
-	fmt.Println(sessionOld)
-	if err = sessionOld.Save(r, w); err != nil {
-		fmt.Printf("Error saving session: %v", err)
-	}
-	http.Redirect(w, r, "/", 302)
->>>>>>> bb93e3e3ef43d51daaec1b296178ff0087282ca2
 }
 
 func sessHandler(w http.ResponseWriter, r *http.Request) {
@@ -153,16 +142,6 @@ func userAllowed(r string, u string) (b bool) {
 	return true
 }
 
-func NoCache(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache, private, max-age=0")
-		w.Header().Set("Expires", time.Unix(0, 0).Format(http.TimeFormat))
-		w.Header().Set("Pragma", "no-cache")
-		w.Header().Set("X-Accel-Expires", "0")
-
-		h.ServeHTTP(w, r)
-	})
-}
 
 func SecuredRoute(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -183,23 +162,14 @@ func main() {
 	go hub.run()
 
 	// setup our routes
-<<<<<<< HEAD
 	mux := mux.NewRouter()
 	// route to the static folder (css/js/img)
-	mux.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
-
+	mux.PathPrefix(STATIC_PATH).Handler(http.StripPrefix(STATIC_PATH, http.FileServer(http.Dir(STATIC_DIR))))
+	// application routes
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/login", loginHandler)
 	mux.HandleFunc("/logout", logoutHandler)
 	mux.HandleFunc("/dashboard", dashboardHandler)
-=======
-	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(homeHandler))
-	mux.Handle("/login", http.HandlerFunc(postHomeHandler))
-	mux.Handle("/logout", http.HandlerFunc(logoutHandler))
-	mux.Handle("/dashboard", NoCache(SecuredRoute(http.HandlerFunc(dashboardHandler))))
-	mux.Handle("/sess", NoCache(SecuredRoute(http.HandlerFunc(sessHandler))))
->>>>>>> bb93e3e3ef43d51daaec1b296178ff0087282ca2
 
 	// websocket route
 	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
